0c8f0e0fbb12eed1e7a02b71d7019dc7d56dc2ba
jakewhy <jwyboyles@gmail.com>
2022-08-18 10:34:22 -0500

    add constructor to create book objects

---
+++ javascript.js
@@ -0,0 +1,27 @@
+const bookOne = {
+    name: "Game of Thrones",
+    author: "George RR Martin",
+    pageCount: "400",
+    haveRead: "Yes",
+}
+
+const bookTwo = {
+    name: "The Lottery and Other Stories",
+    author: "Shirley Jackson",
+    pageCount: "250",
+    haveRead: "Yes",
+}
+
+const bookThree = {
+    name: "Normal People",
+    author: "Sally Rooney",
+    pageCount: "350",
+    haveRead: "No",
+}
+
+function book(name, author, pageCount, haveRead) {
+    this.name = name
+    this.author = author
+    this.pageCount = pageCount
+    this.haveRead = haveRead
+}

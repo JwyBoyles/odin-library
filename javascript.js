5efb588c485735818c36853a4c13ed23f190db98
jakewhy <jwyboyles@gmail.com>
2022-08-19 14:00:41 -0500

    change program to display as cards instead of a table

--- javascript.js
+++ javascript.js
@@ -1,25 +1,6 @@
 let myLibrary = [];
+const container = document.querySelector(".container")
 
-const bookOne = {
-    name: "Game of Thrones",
-    author: "George RR Martin",
-    pageCount: "400",
-    haveRead: "Yes",
-}
-
-const bookTwo = {
-    name: "The Lottery and Other Stories",
-    author: "Shirley Jackson",
-    pageCount: "250",
-    haveRead: "Yes",
-}
-
-const bookThree = {
-    name: "Normal People",
-    author: "Sally Rooney",
-    pageCount: "350",
-    haveRead: "No",
-}
 
 function book(name, author, pageCount, haveRead) {
     this.name = name
@@ -28,8 +9,41 @@ function book(name, author, pageCount, haveRead) {
     this.haveRead = haveRead
 }
 
+bookOne = new book("Game of Thrones", "George RR Martin", "400", "Yes")
+bookTwo = new book("The Lottery and Other Stories", "Shirley Jackson", "250", "Yes")
+bookThree = new book("Normal People", "Sally Rooney", "300", "No")
+
+
 myLibrary.push(bookOne);
 myLibrary.push(bookTwo);
 myLibrary.push(bookThree);
 
-console.log(myLibrary);
+
+
+
+function addBooks () {
+    for (let i = 0; i < myLibrary.length; ++i) {
+        const cardGroup = document.createElement('div');
+        cardGroup.classList.add('cardGroup');
+        let titleLine = document.createElement('div');
+        let authorLine = document.createElement('div');
+        let pageLine = document.createElement('div');
+        let readLine = document.createElement('div');
+        titleLine.classList.add('content');
+        authorLine.classList.add('content');
+        pageLine.classList.add('content');
+        readLine.classList.add('content');
+        cardGroup.appendChild(titleLine);
+        cardGroup.appendChild(authorLine);
+        cardGroup.appendChild(pageLine);
+        cardGroup.appendChild(readLine);
+        let bookCard = myLibrary[i]
+        titleLine.textContent = bookCard.name
+        authorLine.textContent = bookCard.author
+        pageLine.textContent = bookCard.pageCount
+        readLine.textContent = bookCard.haveRead
+        container.appendChild(cardGroup);
+    }
+}
+
+addBooks();

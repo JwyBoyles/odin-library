819c5bc8b2d6d69507e16df2f6c298d3591332ef
jakewhy <jwyboyles@gmail.com>
2022-08-25 14:27:49 -0500

    restart project

--- javascript.js
+++ javascript.js
@@ -1,49 +1,21 @@
 let myLibrary = [];
-const container = document.querySelector(".container")
 
-
-function book(name, author, pageCount, haveRead) {
-    this.name = name
-    this.author = author
-    this.pageCount = pageCount
-    this.haveRead = haveRead
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-bookOne = new book("Game of Thrones", "George RR Martin", "400", "Yes")
-bookTwo = new book("The Lottery and Other Stories", "Shirley Jackson", "250", "Yes")
-bookThree = new book("Normal People", "Sally Rooney", "300", "No")
+let bookOne = new Book("Game of Thrones", "George RR Martin", "520", "Yes")
+let bookTwo = new Book("The Trial", "Franz Kafka", "200", "Yes")
+let bookThree = new Book("The Waves", "Virginia Wolfe", "300", "No")
 
 
-myLibrary.push(bookOne);
-myLibrary.push(bookTwo);
-myLibrary.push(bookThree);
+myLibrary.push(bookOne,bookTwo,bookThree);
 
+console.log(myLibrary);
 
-
-
-function addBooks () {
-    for (let i = 0; i < myLibrary.length; ++i) {
-        const cardGroup = document.createElement('div');
-        cardGroup.classList.add('cardGroup');
-        let titleLine = document.createElement('div');
-        let authorLine = document.createElement('div');
-        let pageLine = document.createElement('div');
-        let readLine = document.createElement('div');
-        titleLine.classList.add('content');
-        authorLine.classList.add('content');
-        pageLine.classList.add('content');
-        readLine.classList.add('content');
-        cardGroup.appendChild(titleLine);
-        cardGroup.appendChild(authorLine);
-        cardGroup.appendChild(pageLine);
-        cardGroup.appendChild(readLine);
-        let bookCard = myLibrary[i]
-        titleLine.textContent = bookCard.name
-        authorLine.textContent = bookCard.author
-        pageLine.textContent = bookCard.pageCount
-        readLine.textContent = bookCard.haveRead
-        container.appendChild(cardGroup);
-    }
+for (let i = 0; i < myLibrary.length; ++i) {
+    
 }
-
-addBooks();

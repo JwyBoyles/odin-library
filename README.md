80ced34f2f0cb1e6f8967398be15ba00d9ad1dd7
jakewhy <jwyboyles@gmail.com>
2022-08-25 14:52:30 -0500

    add table to host library information

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # odin-library
+
+const bookOne = {
+    name: "Game of Thrones",
+    author: "George RR Martin",
+    pageCount: "400",
+    haveRead: "Yes",
+}
+
+const bookTwo = {
+    name: "The Lottery and Other Stories",
+    author: "Shirley Jackson",
+    pageCount: "250",
+    haveRead: "Yes",
+}
+
+const bookThree = {
+    name: "Normal People",
+    author: "Sally Rooney",
+    pageCount: "350",
+    haveRead: "No",
+}
--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+let table = document.querySelector("table");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -14,8 +15,33 @@ let bookThree = new Book("The Waves", "Virginia Wolfe", "300", "No")
 
 myLibrary.push(bookOne,bookTwo,bookThree);
 
-console.log(myLibrary);
+console.log(myLibrary[0].title);
+
+function addTitle () {
+    let cell = document.createElement('td');
+    cell.textContent = bookOne.title;
+    row.appendChild(cell);
+}
 
 for (let i = 0; i < myLibrary.length; ++i) {
-    
+    const row = document.createElement('tr');
+
+    let titleCell = document.createElement('td');
+    titleCell.textContent = myLibrary[i].title;
+
+    let authorCell = document.createElement('td');
+    authorCell.textContent = myLibrary[i].author;
+
+    let pagesCell = document.createElement('td');
+    pagesCell.textContent = myLibrary[i].pages;
+
+    let readCell = document.createElement('td');
+    readCell.textContent = myLibrary[i].read;
+
+    row.appendChild(titleCell);
+    row.appendChild(authorCell);
+    row.appendChild(pagesCell);
+    row.appendChild(readCell);
+
+    table.appendChild(row);
 }
